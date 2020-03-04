c2eb2c217c32e274a668a6ce1cbe488cbab32f29
Kim TaeHun <nuckly60@gmail.com>
2020-03-04 12:51:48 +0900

    Timer css 폰트크기 수정

--- src/App.js
+++ src/App.js
@@ -2,23 +2,23 @@
 import React from 'react';
 import './App.css';
 
-import Navigation from './components/Navigation'
-
+import Navigation from './components/Main/navigation/Navigation'
+import Banner from './components/Main/contents/Banner'
 
 function App(props) {
-  
+
   return (
     <div className="App">
-    <Navigation id="nav"/>
-    <div>
-      HI
-    </div>
+      <Navigation id="nav" />
+
 
       <div id="bottom">
-      GaeTaeng's Home <br/>
-      Naver : 000000@naver.com <br/>
-      GMail : 000000@gmail.com <br/>
+        GaeTaeng's Home <br />
+        Naver : 000000@naver.com <br />
+        GMail : 000000@gmail.com <br />
       </div>
+      <Banner id="banner" />
+
     </div>
   );
 }
---
+++ src/components/Main/contents/Banner.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+function Banner() {
+    return(
+        <div id="banner">
+                HI
+        </div>
+    )
+}
+
+export default Banner
