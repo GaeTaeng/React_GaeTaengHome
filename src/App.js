7fa3999d985dcf8d5ae6fc6b19df45d225c018fe
Kim TaeHun <nuckly60@gmail.com>
2020-03-06 22:30:58 +0900

    Create Contents.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 
 import Navigation from './components/Main/navigation/Navigation'
 import Banner from './components/Main/contents/Banner'
+import Contents from './components/Main/contents/Contents'
 
 function App(props) {
 
@@ -11,13 +12,13 @@ function App(props) {
     <div className="App">
       <Navigation id="nav" />
 
+      <Contents id="contents"/>
 
       <div id="bottom">
         GaeTaeng's Home <br />
         Naver : 000000@naver.com <br />
         GMail : 000000@gmail.com <br />
       </div>
-      <Banner id="banner" />
 
     </div>
   );
---
+++ src/components/Main/contents/Contents.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Banner from './Banner';
+
+function Contents() {
+    return(
+        <div>
+            a
+         <Banner id="banner" />
+        </div>
+    )
+}
+ 
+
+export default Contents;
