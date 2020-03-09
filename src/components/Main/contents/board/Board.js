04f171a08b72af56b603f3c937f0a2b7d5e19a26
Kim TaeHun <nuckly60@gmail.com>
2020-03-09 12:16:28 +0900

    Board& i'll go to buy coffee/temporary storage

--- src/components/Main/contents/board/Board.js
+++ src/components/Main/contents/board/Board.js
@@ -1,30 +1,38 @@
 import React from 'react';
 import "./Board.css";
 
+
+import Table from '@material-ui/core/Table';
+import TableHead from '@material-ui/core/TableHead';
+import TableBody from '@material-ui/core/TableBody';
+
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+
 const post = [
     {
-    'id':1,
-    'title':"1 Post",
-    'content':"내용1",
-    'writer' : "김태훈",
-    'date' : Date.now
-  },
-  {
-    'id':2,
-    'title':"2 Post",
-    'content':"내용2",
-    'writer' : "개탱",
-    'date' : Date.now
-  },
-  {
-    'id':3,
-    'title':"3 Post",
-    'content':"내용3",
-    'writer' : "탱탱",
-    'date' : Date.now
-  },
+        'id': 1,
+        'title': "1 Post",
+        'content': "내용1",
+        'writer': "김태훈",
+        'date': Date.now.toString
+    },
+    {
+        'id': 2,
+        'title': "2 Post",
+        'content': "내용2",
+        'writer': "fmldlmdd",
+        'date': Date.now.toString
+    },
+    {
+        'id': 3,
+        'title': "3 Post",
+        'content': "내용3",
+        'writer': "탱탱",
+        'date': Date.now.toString
+    }
 ]
-  
+
 function Board() {
     return (
         <div>
@@ -38,10 +46,18 @@ function Board() {
 function PostList() {
     return (
         <div>
+        <TableHead>
+            <TableCell>ID</TableCell>
+            <TableCell>TITLE</TableCell>
+            <TableCell>CONTENT</TableCell>
+            <TableCell>WRITER</TableCell>
+            <TableCell>DATE</TableCell>
+        </TableHead>
             {
                 post.map(p => {
-                    return(
+                    return (
                         <PostView id={p.id} title={p.title} content={p.content} writer={p.writer} date={p.date} />
+
                     )
                 })
             }
@@ -50,9 +66,16 @@ function PostList() {
 }
 
 function PostView(props) {
-    return(
+    return (
         <div>
-            <h2>{props.id} {props.title} {props.content} {props.writer} {props.date}</h2>
+            <TableRow>
+
+                <TableCell>{props.id}</TableCell>
+                <TableCell>{props.title}</TableCell>
+                <TableCell>{props.content}</TableCell>
+                <TableCell>{props.writer}</TableCell>
+                <TableCell> {props.date}</TableCell>
+            </TableRow>
         </div>
     )
 }
