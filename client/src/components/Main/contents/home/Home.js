975153dc14f0ae9e3e0b7756c8c0e88118314adb
Kim TaeHun <nuckly60@gmail.com>
2020-04-20 19:54:07 +0900

    check push

--- client/src/components/Main/contents/home/Home.js
+++ client/src/components/Main/contents/home/Home.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Home() {
     return(
         <div>
-            HELLO!WORLD! <br />
+            Hello~ World! <br />
             Welcome GaeTaeng's House! <br />
         </div>
     )
